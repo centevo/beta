1e4145abb1f4909311c011202407f1d065cf9e7d
centevo <144074037+centevo@users.noreply.github.com>
2023-09-14 14:29:30 +0300

    new

--- js/index.js
+++ js/index.js
@@ -1,123 +1,177 @@
+import { preloadImages } from './utils';
 import { gsap } from 'gsap';
+import { ContentItem } from './contentItem';
+import { Flip } from 'gsap/Flip';
+gsap.registerPlugin(Flip);
 
-// DOM elements
-const DOM = {
-    // For demo purposes, trigger the effect when clicking any link in the menu (.line-link)
-    menuLinks: [...document.querySelectorAll('.line-link')],
-    // Cover element (wrap, outer and image inner elements)
-    cover: {
-        wrap: document.querySelector('.cover-wrap'),
-        outer: document.querySelector('.cover'),
-        inner: document.querySelector('.cover__inner'),
-    },
-    // Some of the main page content elements
-    // We'll animate some of the content elements when expanding the menu
-    content: {
-        imgs: [...document.querySelectorAll('.content > .content__img')],
-        titles: [...document.querySelectorAll('.content > .content__title')]
-    },
-    // Menu element (.menu)
-    menu: document.querySelector('.menu'),
-    // Element that slides out
-    menuContent: document.querySelector('.menu__content'),
-    // Close button
-    closeCtrl: document.querySelector('.menu__back'),
-    // Extra elements that will be animated inside the menu
-    extra: document.querySelectorAll('.menu__tagline, .menu__social-author')
-};
+// body
+const body = document.body;
 
-let menuStatus = {
-    isOpen: false,
-    isAnimating: false
-};
+// .content__item elements
+const contentItems = [...document.querySelectorAll('.content__item')];
 
-// Animation gsap timeline
-const menuTimeline = gsap.timeline({
-    paused: true,
-    onComplete: () => menuStatus.isAnimating = false,
-    onReverseComplete: () => menuStatus.isAnimating = false,
-    defaults: {
-        duration: 1.2,
-        ease: 'power4.inOut'
+// For the purposes of the demo (which is to showcase the animation only) we'll hardcode the following.
+const contentItemsArr = [];
+contentItems.forEach(item => contentItemsArr.push(new ContentItem(item)));
+const contentItemCurrent = contentItemsArr.find(item => item.isCurrent());
+
+// .image elements
+const images = [...document.querySelectorAll('.image')];
+
+// total
+const totalImages = images.length;
+
+// .prime
+const prime = document.querySelector('.prime');
+
+// all .image-wrap. This is the original position af an image element bofore moving to the content item (Flip animation)
+const imageWraps = [...prime.querySelectorAll('.image-wrap')];
+
+// View all projects link
+const viewAllProjects = prime.querySelector('.nav__item--active');
+
+// Quote texts that will get hidden when showing the images carousel
+const texts = [...prime.querySelectorAll('.oh > .oh__inner')];
+
+// .nav-wrap element that moves up/down when toggling the view
+const navWrap = document.querySelector('.nav-wrap');
+
+let isAnimating;
+
+// Animation / Show images carousel
+const showImages = () => {
+    
+    if ( isAnimating ) return;
+    isAnimating = true;
+
+    // Save current state of the images
+    const flipstate = Flip.getState(images);
+    
+    const tl = gsap.timeline({
+        onComplete: () => {
+            // pointer events
+            body.classList.add('content-open');
+            isAnimating = false;
+        },
+        defaults: {
+            duration: 1.2,
+            ease: 'power4.inOut',
+        }
+    })
+    .addLabel('start', 0)
+    .add(() => {
+        // Append each image to each content item
+        for (const [pos, contentItem] of contentItems.entries()) {
+            contentItem.appendChild(images[totalImages-1-pos]);
+        }
+        
+        // flip them
+        Flip.from(flipstate, {
+            duration: 1,
+            ease: 'expo.inOut',
+            absolute: true,
+            simple: true,
+            /*
+            spin: pos => {
+                if (pos === totalImages-1) return 0;
+
+                return pos%2 ? 1 : -1;
+            },
+            */
+            stagger: -0.06
+        });
+    }, 'start+=0.15');
+
+    for (const text of texts) {
+        const xy = gsap.utils.random(0,1,1);
+
+        tl.to(text, {
+            xPercent: xy ? gsap.utils.random(0,1,1)%2 ? -100 : 100 : 0,
+            yPercent: xy ? 0 : gsap.utils.random(0,1,1)%2 ? -100 : 100
+        }, 'start');
     }
-})
-.addLabel('start', 0)
-.add(() => {
-    // Add pointer events to auto/none
-    DOM.menu.classList[menuStatus.isOpen ? 'add' : 'remove']('menu--open');
-}, 'start')
-.to(DOM.cover.wrap, {
-    duration: 1.6,
-    startAt: {scale: '1.1'},
-    ease: 'power3.inOut',
-    scale: 1
-}, 'start')
-.to(DOM.cover.outer, {
-    startAt: {y: '-100%'},
-    y: '0%'
-}, 'start')
-.to(DOM.cover.inner, {
-    startAt: {y: '100%'},
-    y: '0%'
-}, 'start')
-.to(DOM.content.imgs, {
-    //ease: 'power3.inOut',
-    y: position => `${position%2 === 0 ? -20 : 20}%`,
-}, 'start')
-.to(DOM.content.titles, {
-    //ease: 'power3.inOut',
-    y: position => `${position%2 === 0 ? 20 : -20}%`,
-}, 'start')
-.addLabel('menu', 0.5)
-.to(DOM.menuContent, {
-    duration: 1,
-    startAt: {y: '-100%'},
-    y: '0%',
-}, 'menu')
-.addLabel('extra', 'menu+=0.6')
-.set(DOM.extra, {
-    y: '400%', 
-    opacity: 0
-}, 'start')
-.to(DOM.extra, {
-    duration: 0.5,
-    ease: 'power4',
-    startAt: {opacity: 1},
-    opacity: 1,
-    y: '0%'
-}, 'extra');
 
-// Menu expand
-const expandMenu = () => {
-    if ( menuStatus.isAnimating || menuStatus.isOpen ) return;
-    menuStatus.isAnimating = true;
-    menuStatus.isOpen = true;
-    menuTimeline.play();
-};
-
-// Menu collapse
-const collapseMenu = () => {
-    if ( menuStatus.isAnimating || !menuStatus.isOpen ) return;
-    menuStatus.isAnimating = true;
-    menuStatus.isOpen = false;
-    menuTimeline.reverse(0);
+    tl
+    .to(navWrap, {
+        yPercent: 120 // a bit more than 100 so it doesn't end right below the viewport
+    }, 'start')
+    .to([contentItemCurrent.DOM.number, contentItemCurrent.DOM.title], {
+        y: 0
+    }, 'start+=0.3')
+    .to(contentItemCurrent.DOM.back, {
+        opacity: 1
+    }, 'start+=0.3');
+    
 }
 
-// Expand the menu when pressing any of the menu top links...
-DOM.menuLinks.forEach(link => {
+// Back animation
+contentItemCurrent.DOM.back.addEventListener('click', () => {
+    
+    if ( isAnimating ) return;
+    isAnimating = true;
 
-    link.addEventListener('click', ev => {
-        ev.preventDefault();
-        expandMenu();
-    });
+    // Save current state of the images
+    const flipstate = Flip.getState(images);
+
+    const tl = gsap.timeline({
+        onComplete: () => {
+            body.classList.remove('content-open');
+            isAnimating = false;
+        },
+        defaults: {
+            duration: 1.2,
+            ease: 'power4.inOut',
+        }
+    })
+    .addLabel('start', 0)
+    .to([contentItemCurrent.DOM.number, contentItemCurrent.DOM.title], {
+        y: '100%'
+    }, 'start')
+    .to(contentItemCurrent.DOM.back, {
+        opacity: 0
+    }, 'start')
+    .to(navWrap, {
+        yPercent: 0
+    }, 'start')
+    .to(texts, {
+        xPercent: 0,
+        yPercent: 0
+    }, 'start')
+    .add(() => {
+        // Put images in their original positions
+        prime.prepend(images[0]);
+        for (const [pos, imageWrap] of imageWraps.entries()) {
+            imageWrap.appendChild(images[pos+1]);
+        }
+        
+        // flip them
+        Flip.from(flipstate, {
+            duration: 1,
+            ease: 'expo.inOut',
+            absolute: true,
+            simple: true,
+            stagger: 0.06
+        });
+    }, 'start+=0.15');
 
 });
 
-// Collapse it when pressing the close button
-DOM.closeCtrl.addEventListener('click', ev => {
-    ev.preventDefault();
-    collapseMenu();
-});
+viewAllProjects.addEventListener('click', () => showImages());
 
+for (const item of contentItemsArr) {
+    if ( !item.isCurrent() ) {
+        item.DOM.el.addEventListener('mouseenter', () => {
+            gsap.to([item.DOM.number, item.DOM.title], {
+                y: 0
+            });
+        });
+        item.DOM.el.addEventListener('mouseleave', () => {
+            gsap.to([item.DOM.number, item.DOM.title], {
+                y: '100%'
+            });
+        });
+    }
+}
 
-
+// Preload images
+preloadImages('.image__inner').then(() => document.body.classList.remove('loading'));
